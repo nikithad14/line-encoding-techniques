c6577b3923f0a71f339a94f9aced1d6d764f7994
nikithad14 <142014339+nikithad14@users.noreply.github.com>
2025-02-07 22:06:20 +0530

    Update line_encoding_techniques.py

--- line_encoding_techniques.py
+++ line_encoding_techniques.py
@@ -1,3 +1,11 @@
+'''2025 Author: Nikitha D <dnikitha2020@gmail.com>'''
+'''
+Dependencies : numpy matplotlib
+Description:
+      This Python script visualizes different line encoding techniques used in digital communication.
+      It takes a binary input and generates signals for NRZ, RZ, Manchester, Differential Manchester, AMI, and Pseudoternary encoding. 
+      The encoded signals are plotted alongside the clock and input signals for easy comparison.
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 data = input("Enter the input bits: ")
